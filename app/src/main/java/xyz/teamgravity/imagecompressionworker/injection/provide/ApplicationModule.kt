af083963ad0ae6177dd9a11338f2bf181fbcff6b
raheemadamboev <vividricemaryandmax@gmail.com>
2023-08-12 17:30:04 +0500

    Provided dependencies;

---
+++ app/src/main/java/xyz/teamgravity/imagecompressionworker/injection/provide/ApplicationModule.kt
@@ -0,0 +1,18 @@
+package xyz.teamgravity.imagecompressionworker.injection.provide
+
+import android.app.Application
+import androidx.work.WorkManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideWorkManager(application: Application): WorkManager = WorkManager.getInstance(application)
+}
